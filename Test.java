b8261a319f64151abea91e9eb2624d414652b100
Sivaprakash R <sivaprakash2k3@gmail.com>
2026-09-22 07:50:00 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -33,10 +33,10 @@ public class Test {
         int x = 590;
         int y = 10;
 
-        add(x, y)
+        add(x, y);
         sub(x, y);
         mul(x, y)
-        div(x, y);
+        div(x, y)
     
     }
 
